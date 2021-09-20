3c092788fe4bed99f856558c9620a653f578bdda
Marc Moten <marcmotenii@gmail.com>
2021-09-20 14:13:54 -0500

    added intro component

---
+++ react-portfolio/src/components/Intro.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Intro.css'
+
+function Intro() {
+    return(
+        <div className='introContainer'>
+            <h1 id="topH1">Hello. I'm Marc,</h1>
+            <h1>A Full-Stack Developer</h1>
+        </div>
+    )
+}
+
+export default Intro;
