e548021a3ac426379dcbcef11219b68f9130e3da
Marc Moten <marcmotenii@gmail.com>
2021-09-19 21:15:53 -0500

    added basic navbar

---
+++ react-portfolio/src/Navbar.js
@@ -0,0 +1,16 @@
+
+
+function Navbar() {
+    return (
+        <div>
+            <div class="navBar">
+        <a href="#Home">Home</a>
+        <a href="#Aboutme">About Me</a>
+        <a href="#Projects">Projects</a>
+        <a href="#Contactme">Contact Me</a>
+    </div>
+        </div>
+    )
+}
+
+export default Navbar;
