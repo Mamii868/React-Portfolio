5708100a0361edfa23b61e394bc504bb423306ef
Marc Moten <marcmotenii@gmail.com>
2021-09-20 16:55:51 -0500

    began Projects section

---
+++ react-portfolio/src/components/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Projects() {
+    return (
+        <projects>
+            <h2 className="title">Projects</h2>
+
+            
+        </projects>
+    )
+}
+
+export default Projects;
