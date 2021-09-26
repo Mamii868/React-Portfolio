73035a396fe9e57cc1cd033ccf2192d619c5bdc1
Marc Moten <marcmotenii@gmail.com>
2021-09-25 21:36:14 -0500

    created button

---
+++ react-portfolio/src/components/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Button.css'
+
+
+export default function Button() {
+    return (
+        <div>
+        <button className="button">
+            To Website
+        </button>
+        </div>
+    )
+};
