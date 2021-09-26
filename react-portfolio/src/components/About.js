988d3c793f9646fdadddaa3ea8ff66cb179a0cd1
Marc Moten <marcmotenii@gmail.com>
2021-09-25 21:36:04 -0500

    rewrote About info

--- react-portfolio/src/components/About.js
+++ react-portfolio/src/components/About.js
@@ -3,17 +3,16 @@ import "./About.css"
 import personalPicture from '../images/PersonalPic-modified.png'
 function About() {
     return (
-        <about classname="aboutDiv">
+        <about classname="AboutDiv" id="About">
             <h2 className= "title">About</h2>
             <imagediv>
                 <img src={personalPicture} alt="Myself" id="personalPicture"/>
             </imagediv>
             <div id="aboutInfo">
-            <p>My name is Marc Moten, an entry level full-stack developer.
-                While learning the basics of game design, I developed a love for coding and continue to learn as much as possible
+            <p>My name is Marc Moten, an entry level full-stack developer. Born in 2002 and graduating from highschool in 2020,
+                the need for more accessible and user-friendly programs pulled me in to the world of web development.
             </p>
             <p>My hobbies include playing the French horn, enjoying pizza, and gaming with friends.</p>
-
             </div>
         </about>
 
