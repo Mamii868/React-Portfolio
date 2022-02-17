06f3c1212c22950800b2887f521b7ef2b63437d8
Marc Moten <marcmotenii@gmail.com>
2022-02-17 15:57:30 -0600

    prettier formatting

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-
-import './App.css';
-import Intro from './components/Intro';
-import Navigation from './components/Navbar';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Skills from './components/Skills';
-import Line from './components/Line';
+import "./App.css";
+import Intro from "./components/Intro";
+import Navigation from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Skills from "./components/Skills";
+import Line from "./components/Line";
 
 function App() {
   return (
-
     <div className="App">
       <Navigation />
       <Intro />
       <div className="topPage">
-      <About />
-      <Line />
-      <Skills/>
-      <Line/>
-      <Projects />
-      <Line/>
-      <Contact/>
-    </div>
+        <About />
+        <Line />
+        <Skills />
+        <Line />
+        <Projects />
+        <Line />
+        <Contact />
+      </div>
     </div>
   );
 }
