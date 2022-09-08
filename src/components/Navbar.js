3a39ee967bc83d165f6845bf3a8ca99c9712705a
Marc Moten <marcmotenii@gmail.com>
2022-09-07 21:13:16 -0500

    Navbar now works on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,27 @@
 import React from "react";
 import "./Navbar.css"
 import Pdf from "../assets/New Resume.pdf"
-import { Navbar } from "react-bootstrap";
-import { Container } from "react-bootstrap";
-import { Nav } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 function Navigation() {
   function onResumeClick() {
     window.open(Pdf);
   };
     return (
-      <Navbar bg="dark" variant="dark">
-    <Container>
+  <Navbar bg="dark" expand="lg" variant="dark">
+  <Container>
     <Navbar.Brand href="#home">Marc Moten</Navbar.Brand>
-    <Nav className="me-auto">
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
       <Nav.Link href="#About">About</Nav.Link>
       <Nav.Link href="#Skills">Skills</Nav.Link>
       <Nav.Link href="#Projects">Projects</Nav.Link>
       <Nav.Link href="#Contact">Contact Me</Nav.Link>
       <Nav.Link onClick={onResumeClick}>Resume</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
     )
 }
 
